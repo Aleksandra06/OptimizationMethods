2004b6d6d59edbb1d1f16d9c561defed444e201b
Aleksandra06 <plyaskina-sasha@mail.ru>
2022-12-16 21:36:44 +0700

    add bfs and transport task

---
+++ OptimizationMethods/BFS/Program.cs
@@ -0,0 +1,325 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BFS
+{
+    public class Program
+    {
+        public class GrafModel
+        {
+            public int A { get; set; }//из
+            public int B { get; set; }//в
+            public int W { get; set; }//вec
+        }
+        static void Main(string[] args)
+        {
+            var path = "graf.txt";
+            var data = File.ReadAllLines(path);
+            var graf = GenerationGraf();
+             AddNode(graf, 2, 3009, 2);
+             AddNode(graf, 2003, 2019, 13);
+            //var graf = new Dictionary<int, List<GrafModel>>();
+            //foreach (var row in data)
+            //{
+            //    var tmp = new List<int>();
+            //    var col = row.Split("\t");
+            //    foreach (var item in col)
+            //    {
+            //        tmp.Add(int.Parse(item));
+            //    }
+            //    if (!graf.ContainsKey(tmp[0] - 1))
+            //    {
+            //        graf.Add(tmp[0]-1, new List<GrafModel>() { new GrafModel() { A = tmp[0] - 1, B = tmp[1]-1, W = tmp[2] } });
+            //    }
+            //    else
+            //    {
+            //        var list = graf[tmp[0] - 1];
+            //        list.Add(new GrafModel() { A = tmp[0]-1, B = tmp[1] - 1 , W = tmp[2] });
+            //        graf[tmp[0]-1] = list;
+            //    }
+            //}
+            var startNode = 0;
+            var endNode = 999999;
+            Run(graf, startNode, endNode);
+        }
+
+        private static void AddNode(Dictionary<int, List<GrafModel>> graf, int a, int b, int w)
+        {
+            if (!graf.ContainsKey(a))
+            {
+                graf.Add(a, new List<GrafModel>() { new GrafModel() { A = a, B = b, W = w } });
+            }
+            else
+            {
+                var list = graf[a];
+                list.Add(new GrafModel() { A = a, B = b, W = w });
+                graf[a] = list;
+            }
+        }
+
+        private static Dictionary<int, List<GrafModel>> GenerationGraf()
+        {
+            var graf = new Dictionary<int, List<GrafModel>>();
+            var rows = 1000;
+            var cols = 1000;
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    GrafModel tmp = null;
+                    var item = (row * cols) + col;
+                    if (col + 1 < cols)
+                    {
+                        tmp = new GrafModel() { A = item, B = item + 1, W = 1 };
+                    }
+                    if (tmp != null)
+                    {
+                        if (!graf.ContainsKey(tmp.A))
+                        {
+                            graf.Add(tmp.A, new List<GrafModel>() { tmp });
+                        }
+                        else
+                        {
+                            var list = graf[tmp.A];
+                            list.Add(tmp);
+                            graf[tmp.A] = list;
+                        }
+                    }
+                    tmp = null;
+                    if (row + 1 < rows)
+                    {
+                        tmp = new GrafModel() { A = item, B = item + cols, W = 1 };
+                    }
+                    if (tmp != null)
+                    {
+                        if (!graf.ContainsKey(tmp.A))
+                        {
+                            graf.Add(tmp.A, new List<GrafModel>() { tmp });
+                        }
+                        else
+                        {
+                            var list = graf[tmp.A];
+                            list.Add(tmp);
+                            graf[tmp.A] = list;
+                        }
+                    }
+                }
+            }
+            return graf;
+        }
+        private static void Run(Dictionary<int, List<GrafModel>> graf, int startNode, int endNode)//для 1000*1000
+        {
+            var n = 1000 * 1000;
+
+            var puti = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                puti.Add(i, -1);
+            }
+            puti[startNode] = startNode;
+            Queue<int> q = new Queue<int>();
+            var mark = InitBoolList(n);
+            var d = InitIntList(n);
+            q.Enqueue(startNode);
+            d[startNode] = 0;
+            mark[startNode] = true;
+            var id = 0;
+            //var sloys = new List<List<int>>() { new List<int>() { startNode } };
+            //sloys.Add(new List<int>());
+            //var sloysId = 1;
+            //Console.Write("\n" + sloysId + ": " + (startNode + 1));
+            var flag = false;
+            while (q.Count > 0 && !flag)
+            {
+                if (id % 100000 == 0)
+                {
+                    //  Console.WriteLine(id);
+                }
+                var v = q.Dequeue();
+                //Console.Write("\n" + (v + 1) + ":");
+                id++;
+                var listsosed = graf.ContainsKey(v) ? graf[v].ToList() : null;
+                if (listsosed == null)
+                {
+                    continue;
+                }
+                for (int i = 0; i < listsosed.Count; i++)
+                {
+                    if (mark[listsosed[i].B] == false || (d[v] + listsosed[i].W) < d[listsosed[i].B])
+                    {
+                        d[listsosed[i].B] = d[v] + listsosed[i].W;
+                        mark[listsosed[i].B] = true;
+                        q.Enqueue(listsosed[i].B);
+                        puti[listsosed[i].B] = v;
+                        //if (listsosed[i].B == endNode)
+                        //{
+                        //    flag = true;
+                        //}
+                        //sloys.Last().Add(listsosed[i].B);
+                        //Console.Write(", " + (q.Last() + 1));
+                    }
+                }
+                //q.Remove(v);
+                //if (v == sloys[sloysId - 1].LastOrDefault())
+                //{
+                //    //if (sloys.Last().Count() > 0)
+                //    //{
+                //    //    Console.Write("\n" + (sloys.Count()) + ": ");
+                //    //}
+                //    //foreach (var l in sloys.Last())
+                //    //{
+                //    //    Console.Write(", " + (l + 1));
+                //    //}
+                //    sloys.Add(new List<int>());
+                //    sloysId++;
+                //}
+            }
+
+
+            //Console.Write("\n\n");
+            //for (int i = 0; i < d.Count; i++)
+            //{
+            //    Console.WriteLine((i + 1) + "=" + d[i]);
+            //}
+            //Console.Write("\n\n");
+            //for (int i = 0; i < q.Count; i++)
+            //{
+            //    Console.Write(", " + (q[i] + 1));
+            //}
+            //Console.Write("\n");
+
+            var put = new List<int>();
+            Console.Write("\n" + (endNode + 1) + ":");
+            var node = endNode;
+            while (node != startNode)
+            {
+                put.Add(node);
+                node = puti[put.Last()];
+            }
+            put.Add(startNode);
+            put.Reverse();
+            var s = 0;
+            for (int i = 0; i < put.Count; i++)
+            {
+                Console.Write(", " + (put[i] + 1));
+                s++;
+            }
+            Console.Write("\nСтоимость " + d[endNode]);
+        }
+        private static void Run(List<GrafModel> graf, int startNode)
+        {
+            var a = graf.Select(x => x.A).Max();
+            var b = graf.Select(x => x.B).Max();
+            var n = a > b ? a : b;
+
+            //var puti = new List<List<int>>();
+            //for (int i = 0; i < n + 1; i++)
+            //{
+            //    puti.Add(new List<int>() { startNode });
+            //}
+            var q = new List<int>();
+            var mark = InitIntList(n + 1);
+            var d = InitIntList(n + 1);
+            q.Add(startNode);
+            d[startNode] = 0;
+            mark[startNode] = 1;
+            var id = 0;
+            var sloys = new List<List<int>>() { new List<int>() { startNode } };
+            sloys.Add(new List<int>());
+            var sloysId = 1;
+            //Console.Write("\n" + sloysId + ": " + (startNode + 1));
+            while (q.Count > id)
+            {
+                if (id % 100 == 0)
+                {
+                    Console.WriteLine(id);
+                }
+                var v = q[id];
+                //Console.Write("\n" + (v + 1) + ":");
+                id++;
+                var listsosed = graf.Where(x => x.A == v && !q.Any(y => y == x.B)).ToList();
+                for (int i = 0; i < listsosed.Count; i++)
+                {
+                    if (mark[listsosed[i].B] == 0)
+                    {
+                        d[listsosed[i].B] = d[v] + 1;
+                        mark[listsosed[i].B] = 1;
+                        q.Add(listsosed[i].B);
+                        //puti[listsosed[i].B] = puti[v].ToList();
+                        //puti[listsosed[i].B].Add(listsosed[i].B);
+                        sloys.Last().Add(listsosed[i].B);
+                        //Console.Write(", " + (q.Last() + 1));
+                    }
+                }
+                if (v == sloys[sloysId - 1].LastOrDefault())
+                {
+                    //if (sloys.Last().Count() > 0)
+                    //{
+                    //    Console.Write("\n" + (sloys.Count()) + ": ");
+                    //}
+                    //foreach (var l in sloys.Last())
+                    //{
+                    //    Console.Write(", " + (l + 1));
+                    //}
+                    sloys.Add(new List<int>());
+                    sloysId++;
+                }
+            }
+
+            Console.Write("\n\n");
+            for (int i = 0; i < d.Count; i++)
+            {
+                Console.WriteLine((i + 1) + "=" + d[i]);
+            }
+            Console.Write("\n\n");
+            for (int i = 0; i < q.Count; i++)
+            {
+                Console.Write(", " + (q[i] + 1));
+            }
+            Console.Write("\n");
+
+            //int putid = 1;
+            //foreach (var put in puti)
+            //{
+            //    Console.Write("\n" + putid + ":");
+            //    putid++;
+            //    for (int i = 0; i < put.Count; i++)
+            //    {
+            //        Console.Write(", " + (put[i] + 1));
+            //    }
+            //}
+        }
+        static List<int> InitIntList(int count)
+        {
+            var list = new List<int>();
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(0);
+            }
+            return list;
+        }
+        static List<bool> InitBoolList(int count)
+        {
+            var list = new List<bool>();
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(false);
+            }
+            return list;
+        }
+        static List<List<int>> InitMas(int n, int m)
+        {
+            var mass = new List<List<int>>();
+            for (int i = 0; i < n; i++)
+            {
+                var tmp = new List<int>();
+                for (int j = 0; j < m; j++)
+                {
+                    tmp.Add(0);
+                }
+                mass.Add(tmp);
+            }
+
+            return mass;
+        }
+    }
+}
---
+++ OptimizationMethods/TransportTask/Program.cs
@@ -0,0 +1,306 @@
+﻿namespace TransportTask
+{
+    internal class Program
+    {
+        public class GrafModel
+        {
+            public int C { get; set; }//склад
+            //public int CW { get; set; }//склад вес
+            public int M { get; set; }//магазин
+            //public int MW { get; set; }//магазин вес
+            public int W { get; set; }//вес
+            public int Number { get; set; }//кол-вод
+            //public int U { get; set; }//кол-вод
+            //public int V { get; set; }//кол-вод
+        }
+        static void Main(string[] args)
+        {
+            var data = File.ReadAllLines("graf.txt");
+            var graf = new List<GrafModel>();
+            foreach (var row in data)
+            {
+                var tmp = new List<int>();
+                var col = row.Split("\t");
+                foreach (var item in col)
+                {
+                    tmp.Add(int.Parse(item));
+                }
+                graf.Add(new GrafModel()
+                {
+                    C = tmp[0],
+                    M = tmp[1],
+                    W = tmp[2],
+                    //CW = -1,
+                    //MW = -1,
+                    Number = -1
+                });
+            }
+            data = File.ReadAllLines("magazin.txt");
+            var M = new List<int>();
+            foreach (var row in data)
+            {
+                M.Add(int.Parse(row));
+                var index = graf.FindIndex(x => x.M == M.Count - 1);
+                //graf[index].MW = M.Last();
+            }
+            data = File.ReadAllLines("sklad.txt");
+            var C = new List<int>();
+            foreach (var row in data)
+            {
+                C.Add(int.Parse(row));
+                var index = graf.FindIndex(x => x.C == C.Count - 1);
+                //graf[index].CW = C.Last();
+            }
+            Run(C, M, graf);
+        }
+
+        private static void Run(List<int> C, List<int> M, List<GrafModel> graf)
+        {
+            Ravn(C, M, graf);
+            CalcGrafOne(graf, C.ToList(), M.ToList());
+            var shag = 1;
+            while (true)
+            {
+                Console.WriteLine($"\nШаг {shag}:\n");
+                var zanyat = graf.Where(x => x.Number > 0).Count();
+                Console.WriteLine($"Занятых клеток: {zanyat}; n + m - 1 = {C.Count + M.Count - 1}\n");
+                List<int> U = InitIntList(C.Count());
+                List<int> V = InitIntList(M.Count());
+                CalcUV(graf, U, V);
+                PrintGraf(C, M, U, V, graf);
+                var indexMax = FingIndexMax(graf, U, V);
+                if (indexMax == -1)
+                {
+                    break;
+                }
+                //var table = CreateTable(C, M);
+                //PrintTable(table, C, M);
+            }
+        }
+
+        private static int FingIndexMax(List<GrafModel> graf, List<int> u, List<int> v)//u->c, v-> m
+        {
+            var index = -1;
+            var max = -1;
+            for (var i = 0; i < graf.Count(); i++)
+            {
+                var g = graf[i];
+                if (g.Number > 0)
+                {
+                    var tmp = u[g.C] + v[g.M] - g.W;
+                    if (tmp > 0 && max < tmp)
+                    {
+                        index = i;
+                        max = tmp;
+                    }
+                }
+            }
+
+            return index;
+        }
+
+        private static void CalcUV(List<GrafModel> graf, List<int> U, List<int> V)//u->c, v-> m
+        {
+            Queue<int> q = new Queue<int>();
+            for (int i = 0; i < graf.Count; i++)
+            {
+                q.Enqueue(i);
+            }
+            while (q.Count > 0)
+            {
+                var id = q.Dequeue();
+                var g = graf[id];
+                if (g.Number <= 0)
+                {
+                    continue;
+                }
+                if (g.M == 0)
+                {
+                    V[g.M] = 0;
+                    U[g.C] = g.W;
+                }
+                else
+                {
+                    if (V[g.M] == int.MinValue && U[g.C] == int.MinValue)
+                    {
+                        q.Enqueue(id);
+                    }
+                    else if (V[g.M] == int.MinValue)
+                    {
+                        V[g.M] = g.W - U[g.C];
+                    }
+                    else if (U[g.C] == int.MinValue)
+                    {
+                        U[g.C] = g.W - V[g.M];
+                    }
+                }
+            }
+        }
+
+        private static void PrintGraf(List<int> c, List<int> m, List<int> u, List<int> v, List<GrafModel> graf)//u->c, v-> m
+        {
+            Console.Write("\t");
+            for (int j = 0; j < m.Count; j++)
+            {
+                Console.Write(m[j] + "\t");
+            }
+            Console.Write("U\t");
+            var group = graf.GroupBy(x => x.C).ToList();
+            for (int i = 0; i < c.Count(); i++)
+            {
+                Console.WriteLine("\n");
+                Console.Write(Print(c[i]) + "\t");
+                var g = group[i].ToList();
+                for (int j = 0; j < m.Count(); j++)
+                {
+                    var index = g.FindIndex(x => x.M == j);
+                    if (index >= 0 && g[index].Number >= 0)
+                    {
+                        Console.Write($"{g[index].Number}({g[index].W})\t");
+                    }
+                    else
+                    {
+                        Console.Write("-\t");
+                    }
+                }
+                Console.Write(u[i] + "\t");
+            }
+            Console.Write("\nV\t");
+            for (int j = 0; j < v.Count; j++)
+            {
+                Console.Write(Print(v[j]) + "\t");
+            }
+            Console.WriteLine("\n");
+        }
+
+        static string Print(int number)
+        {
+            if (number == int.MinValue)
+            {
+                return "-";
+            }
+            else { return number.ToString(); }
+        }
+
+        private static void CalcGrafOne(List<GrafModel> graf, List<int> C, List<int> M)
+        {
+            foreach (var g in graf)
+            {
+                if (C[g.C] != 0 && M[g.M] != 0)
+                {
+                    if (C[g.C] > M[g.M])
+                    {
+                        g.Number = M[g.M];
+                        C[g.C] = C[g.C] - M[g.M];
+                        M[g.M] = 0;
+                    }
+                    else
+                    {
+                        g.Number = C[g.C];
+                        M[g.M] = M[g.M] - C[g.C];
+                        C[g.C] = 0;
+                    }
+                }
+            }
+        }
+
+        private static void PrintTable(List<List<int>> table, List<int> c, List<int> m)
+        {
+            Console.Write("\t");
+            for (int j = 0; j < m.Count; j++)
+            {
+                Console.Write(m[j] + "\t");
+            }
+            for (int i = 0; i < c.Count; i++)
+            {
+                Console.Write('\n');
+                Console.Write(c[i] + "\t");
+                for (int j = 0; j < m.Count; j++)
+                {
+                    Console.Write(table[i][j] + "\t");
+                }
+            }
+            Console.Write("\n");
+        }
+        private static List<List<int>> CreateTable(List<int> c, List<int> m)
+        {
+            var C = c.ToList();
+            var M = m.ToList();
+            List<List<int>> table = new List<List<int>>();
+            for (int i = 0; i < C.Count; i++)
+            {
+                table.Add(new List<int>());
+                for (int j = 0; j < M.Count; j++)
+                {
+                    if (M[j] <= C[i])
+                    {
+                        table.Last().Add(M[j]);
+                        M[j] = 0;
+                        C[i] = C[i] - M[j];
+                    }
+                    else
+                    {
+                        table.Last().Add(C[i]);
+                        M[j] = M[j] - C[i];
+                        C[i] = 0;
+                    }
+                }
+            }
+            return table;
+        }
+
+        private static void Ravn(List<int> c, List<int> m, List<GrafModel> graf)
+        {
+            if (c.Sum() > m.Sum())
+            {
+                m.Add(c.Sum() - m.Sum());
+                for (int i = 0; i < c.Count; i++)
+                {
+                    graf.Add(new GrafModel() { C = i, M = m.Count() - 1, W = 0, Number = -1 });
+                }
+            }
+            else if (c.Sum() < m.Sum())
+            {
+                c.Add(m.Sum() - c.Sum());
+                for (int i = 0; i < m.Count; i++)
+                {
+                    graf.Add(new GrafModel() { C = c.Count() - 1, M = i, W = 0, Number = -1 });
+                }
+            }
+        }
+
+        static List<int> InitIntList(int count)
+        {
+            var list = new List<int>();
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(int.MinValue);
+            }
+            return list;
+        }
+        static List<bool> InitBoolList(int count)
+        {
+            var list = new List<bool>();
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(false);
+            }
+            return list;
+        }
+        static List<List<int>> InitMas(int n, int m)
+        {
+            var mass = new List<List<int>>();
+            for (int i = 0; i < n; i++)
+            {
+                var tmp = new List<int>();
+                for (int j = 0; j < m; j++)
+                {
+                    tmp.Add(0);
+                }
+                mass.Add(tmp);
+            }
+
+            return mass;
+        }
+    }
+}
